3a4cb968f03fee6c1c912423b3985e730ea9473c
cholle13 <chrisaholle@gmail.com>
2020-02-06 16:07:24 -0600

    Form layout Component / Login

--- src/pages/dashboard/login.js
+++ src/pages/dashboard/login.js
@@ -13,11 +13,28 @@
  */
 
 import React from "react"
-import Layout from "../../components/layout";
-import PageTitle from "../../components/page-title";
+import FormLayout from "../../components/formLayout";
+import FormTitle from "../../components/formTitle";
+import {NormalField} from "reactstrap-form-fields";
+import Button from "../../components/btn";
+import styled from "styled-components";
+import logo from "../../assets/images/BU_BrandMark_Stacked_Gold.png"
+
+const Logo = styled.img`
+    display: inline-block;
+    height: 4em;
+`
 
 export default () => (
-    <Layout>
-        <PageTitle title="Sign In"/>
-    </Layout>
+    <FormLayout>
+        <Logo src={logo}/>
+        <FormTitle title={'Login'}/>
+        <form name="Contact Form" method="POST" netlify>
+            <input type="hidden" name="form-name" value="Contact Form"/>
+            <NormalField name="Email"/>
+            <NormalField name="Password"/>
+            <br/>
+            <Button type="submit" style={{margin: 'auto'}}>Submit</Button>
+        </form>
+    </FormLayout>
 )
