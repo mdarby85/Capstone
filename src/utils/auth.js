319df4497bf68e11df547d4c1cf585c562d2d93f
Brandon Wilcox <bkwilcox100@gmail.com>
2020-04-06 17:58:17 -0500

    Completed and Tested UserManager

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -22,14 +22,16 @@
 
 import auth0 from "auth0-js"
 import { navigate } from "gatsby"
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 const isBrowser = typeof window !== "undefined"
 
 const auth = isBrowser
   ? new auth0.WebAuth({
-      domain: "bkwilcox.auth0.com",
-      clientID: "M6em8UGXUK5zgSV4CwxWTgiGPe97wc5j",
-      redirectUri: "http://localhost:8000/callback",
+      domain: process.env.AUTH0_DOMAIN,
+      clientID: process.env.AUTH0_CLIENTID,
+      redirectUri: process.env.AUTH0_CALLBACK,
       responseType: "token id_token",
       scope: "openid profile email",
     })
